f37c472a3a8677d4c193f5459cd102c4779cd232
Adriii773 <adri.garfan05@gmail.com>
2026-06-28 14:54:54 +0200

    archivos de Logs creados + mast + ts + tmtc_icd+ test procedure - proyecto mal colocado

--- it_asw_ccdronemng_freertos_hito4/Debug/service_libraries/crc/src/crc.d
+++
@@ -1,5 +0,0 @@
-service_libraries/crc/src/crc.o: ../service_libraries/crc/src/crc.c \
- /home/atcsol/eclipse-workspace_FIE/obdh_proy_26_Adrian_Garcia_Afan_de_Rivera/it_asw_ccdronemng_freertos_hito4/service_libraries/crc/include/public/crc.h \
- /home/atcsol/eclipse-workspace_FIE/obdh_proy_26_Adrian_Garcia_Afan_de_Rivera/it_asw_ccdronemng_freertos_hito4/llsw/config/include/public/basic_types.h
-/home/atcsol/eclipse-workspace_FIE/obdh_proy_26_Adrian_Garcia_Afan_de_Rivera/it_asw_ccdronemng_freertos_hito4/service_libraries/crc/include/public/crc.h:
-/home/atcsol/eclipse-workspace_FIE/obdh_proy_26_Adrian_Garcia_Afan_de_Rivera/it_asw_ccdronemng_freertos_hito4/llsw/config/include/public/basic_types.h:
